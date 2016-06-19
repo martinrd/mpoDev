9b994f33481224263a3f1290566496e65e2a1459
Doug Martin <martinrd@comcast.net>
2016-06-18 19:32:59 -0700

    First commit.

---
+++ R/tsPlotMP.R
@@ -0,0 +1,13 @@
+require(lattice)
+tsPlotMP = function(ret,add.grid = F,cex = 1.0, layout = NULL,type = "l",
+                    pct = 100, yname = "RETURNS (%)",scaleType = "free",
+                    lwd = 1, color = "black")
+{
+  if(add.grid) {type = c("l","g")} else
+  {type = type}
+  xyplot(pct*ret,par.strip.text = list(cex = cex),type = type,
+         xlab="", ylab = list(label = yname,cex = cex), lwd = lwd,
+         scales = list(y = list(cex = cex,relation=scaleType),
+                       x = list(cex = cex)),layout = layout,
+                       col = color, strip = F, strip.left = T) 
+}
